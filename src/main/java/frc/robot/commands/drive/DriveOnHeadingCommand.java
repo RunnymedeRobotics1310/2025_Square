08d9395623d1d02c8c6ba8660ba8d7e55e462eb0
Quentin Field <q@quentinfield.com>
2024-12-02 17:14:57 -0500

    finished driveonheading command

--- src/main/java/frc/robot/commands/drive/DriveOnHeadingCommand.java
+++ src/main/java/frc/robot/commands/drive/DriveOnHeadingCommand.java
@@ -1,23 +1,26 @@
 package frc.robot.commands.drive;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.commands.LoggingCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class DriveOnHeadingCommand extends LoggingCommand {
 
-    private double               desiredHeading;
-    private long                 time;
+    private Rotation2d           desiredHeading;
+    private long                 durationMs;
     private final DriveSubsystem driveSubsystem;
     private double               leftSpeed;
     private double               rightSpeed;
     private double               turn;
-    private double               robotHeading;
+    private Rotation2d           robotHeading;
     private double               offset;
+    private long                 startTimeMs;
+    private double               P_VALUE = 0.05;
 
-    public DriveOnHeadingCommand(int speed, double heading, long time, DriveSubsystem driveSubsystem) {
+    public DriveOnHeadingCommand(int speed, Rotation2d heading, long durationMs, DriveSubsystem driveSubsystem) {
 
         this.desiredHeading = heading;
-        this.time           = time;
+        this.durationMs     = durationMs;
         this.driveSubsystem = driveSubsystem;
         this.leftSpeed      = speed;
         this.rightSpeed     = speed;
@@ -28,28 +31,29 @@ public class DriveOnHeadingCommand extends LoggingCommand {
     @Override
     public void initialize() {
         logCommandStart();
+        startTimeMs = System.currentTimeMillis();
     }
 
     @Override
     public void execute() {
         robotHeading = driveSubsystem.getHeading();
 
-        offset       = (desiredHeading - robotHeading) % 360;
+        offset       = (desiredHeading.getDegrees() - robotHeading.getDegrees()) % 360;
 
 
 
-        driveSubsystem.setMotorSpeeds(leftSpeed + offset, rightSpeed - offset);
+        driveSubsystem.setMotorSpeeds(leftSpeed + offset * P_VALUE, rightSpeed - offset * P_VALUE);
 
     }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return System.currentTimeMillis() - startTimeMs >= durationMs;
     }
 
     @Override
     public void end(boolean interrupted) {
-
+        driveSubsystem.stop();
     }
 
 }
