5794de570a4ea188960bfa368303a4874bc8194b
CarsonH <carson.honeyman@gmail.com>
2024-12-09 16:26:35 -0500

    heading

---
+++ src/main/java/frc/robot/commands/drive/DriveOnHeadingCommand.java
@@ -0,0 +1,60 @@
+package frc.robot.commands.drive;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.commands.LoggingCommand;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class DriveOnHeadingCommand extends LoggingCommand {
+
+    private final DriveSubsystem driveSubsystem;
+    private long                 startTimeMillis;
+    private long                 durationMillis;
+    private double               speed;
+
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param driveSubsystem The subsystem used by this command.
+     */
+    public DriveOnHeadingCommand(Rotation2d desiredHeading, double speed, long durationMillis, DriveSubsystem driveSubsystem) {
+
+        this.driveSubsystem = driveSubsystem;
+
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(driveSubsystem);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        logCommandStart();
+        startTimeMillis = System.currentTimeMillis();
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        // The default drive command never ends, but can be interrupted by other commands.
+
+        return System.currentTimeMillis() - startTimeMillis > durationMillis;
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        logCommandEnd(interrupted);
+        driveSubsystem.setMotorSpeeds(0, 0);
+    }
+
+    @Override
+    public void execute() {
+        driveSubsystem.setMotorSpeeds(speed, speed);
+    }
+
+
+
+}
