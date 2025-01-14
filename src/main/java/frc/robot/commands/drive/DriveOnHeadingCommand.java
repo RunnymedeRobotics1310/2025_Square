31436bc7b3d0aae49f9290e3fa7ab3927b4ee8d5
CarsonH <carson.honeyman@gmail.com>
2025-01-14 16:46:41 -0500

    much stuff

--- src/main/java/frc/robot/commands/drive/DriveOnHeadingCommand.java
+++ src/main/java/frc/robot/commands/drive/DriveOnHeadingCommand.java
@@ -14,6 +14,8 @@ public class DriveOnHeadingCommand extends LoggingCommand {
     private Rotation2d           desiredHeading;
     private double               error;
     private double               turnSpeed;
+    private long                 driveingStartTime;
+    private boolean              isTurningComplete;
 
 
     /**
@@ -48,7 +50,16 @@ public class DriveOnHeadingCommand extends LoggingCommand {
     public boolean isFinished() {
         // The default drive command never ends, but can be interrupted by other commands.
         durationMillis = 10000;
-        return System.currentTimeMillis() - startTimeMillis > durationMillis;
+        if (System.currentTimeMillis() - driveingStartTime > 2000 && isTurningComplete) {
+            return true;
+        }
+        if (System.currentTimeMillis() - startTimeMillis > durationMillis) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 
     // Called once the command ends or is interrupted.
@@ -72,27 +83,47 @@ public class DriveOnHeadingCommand extends LoggingCommand {
         }
 
         // Ensure the turn speed is proportional to the error
-        double turnSpeed = 0.8 * Math.abs(error) / 180;
-        turnSpeed = Math.max(turnSpeed, 0.15);
+        turnSpeed = 0.9 * Math.abs(error) / 180;
+        turnSpeed = Math.max(turnSpeed, 0.1);
 
-        // Log the desired and current heading
+        // Log the desired, current heading, and error
         log("desired: " + desiredHeading.getDegrees());
         log("heading: " + driveSubsystem.getHeading().getDegrees());
+        log("Error: " + error);
 
         // If the robot is at the position, move on heading, if not rotate to heading
-        if (turnSpeed > 0.1 && error > 5) {
-            // If the error is positive (meaning we need to turn left), set motor speeds accordingly
-            if (error > 0) {
-                // Turn counter-clockwise (left motor negative, right motor positive)
-                driveSubsystem.setMotorSpeeds(-turnSpeed, turnSpeed);
-            }
-            else {
-                // Turn clockwise (left motor positive, right motor negative)
-                driveSubsystem.setMotorSpeeds(turnSpeed, -turnSpeed);
+        if (turnSpeed > 0.1) {
+            if (error > 1 || error < -1) {
+                // If the error is positive (meaning we need to turn left), set motor speeds
+                // accordingly
+                if (error > 0) {
+                    // Turn counter-clockwise (left motor negative, right motor positive)
+                    driveSubsystem.setMotorSpeeds(-turnSpeed, turnSpeed);
+                }
+                else {
+                    // Turn clockwise (left motor positive, right motor negative)
+                    driveSubsystem.setMotorSpeeds(turnSpeed, -turnSpeed);
+                }
             }
         }
         else {
-            driveSubsystem.setMotorSpeeds(0, 0);
+            if (error < 1 || error > -1) {
+                if (!isTurningComplete) {
+                    isTurningComplete = true;
+                    driveingStartTime = System.currentTimeMillis();
+                }
+                if (error > 5) {
+                    driveSubsystem.setMotorSpeeds(speed - turnSpeed * 0.5, speed);
+                }
+                else {
+                    if (error < 0) {
+                        driveSubsystem.setMotorSpeeds(speed, speed - turnSpeed * 0.5);
+                    }
+                    else {
+                        driveSubsystem.setMotorSpeeds(speed, speed);
+                    }
+                }
+            }
         }
 
     }
