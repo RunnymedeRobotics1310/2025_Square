a6f9682fae8ce2ba359c342d219f17788d45819e
Richard McMullin <richard.e.mcmullin@gmail.com>
2025-02-02 11:57:10 -0500

    Add commments and FIXMEs

--- src/main/java/frc/robot/commands/coral/ScoreL4Command.java
+++ src/main/java/frc/robot/commands/coral/ScoreL4Command.java
@@ -7,6 +7,7 @@ import frc.robot.subsystems.CoralSubsystem;
 public class ScoreL4Command extends LoggingCommand {
 
     private final CoralSubsystem coralSubsystem;
+    // FIXME: move the height to the Constants class and make constants for all of the heights.
     private final double         l4Height          = 20;
     private final double         elevatorTolerance = 1;
 
@@ -25,8 +26,16 @@ public class ScoreL4Command extends LoggingCommand {
 
     @Override
     public void execute() {
+
+        // FIXME: This will cause the elevator to always go up a little bit even when
+        // at the target because we are setting the elevator speed twice (once here (always),
+        // and once below (when we are at the target). The motor will appear to stutter.
+        // Instead, calculate the motor speed that you want (up, down, or zero), and then
+        // set it once at the end of the execute loop.
         coralSubsystem.setElevatorSpeed(0.5);
 
+        // TODO: While this is correct, normally a tolerance would be checked by
+        // if (Math.abs(actual - target) <= tolerance) {
         if (coralSubsystem.getElevatorEncoder() <= l4Height + elevatorTolerance &&
             coralSubsystem.getElevatorEncoder() >= l4Height - elevatorTolerance) {
             coralSubsystem.setElevatorSpeed(0);
@@ -35,7 +44,14 @@ public class ScoreL4Command extends LoggingCommand {
 
     @Override
     public boolean isFinished() {
+
+        // FIXME:
+        // set the end reason and the end routine will log it for you
+        // setFinishReason("the reason text");
+        // Also, only set the reason once we know the command is finished.
         log("AT L4");
+
+        // FIXME: When should this command end?
         return false;
 
     }
