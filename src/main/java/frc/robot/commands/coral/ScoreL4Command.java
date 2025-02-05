afbd181aa2c365715d56e43b41c0488e48fa34b2
Richard McMullin <richard.e.mcmullin@gmail.com>
2025-02-05 15:00:18 -0500

    FIXMEs

--- src/main/java/frc/robot/commands/coral/ScoreL4Command.java
+++ src/main/java/frc/robot/commands/coral/ScoreL4Command.java
@@ -24,10 +24,13 @@ public class ScoreL4Command extends LoggingCommand {
     @Override
     public void execute() {
 
+        // FIXME: how do we determine whether the elevator should go up or down?
+
         // sets the elevator speed to the elevator speed constant
         coralSubsystem.setElevatorSpeed(CoralConstants.ELEVATOR_SPEED);
 
         // checks to see if the elevator is at the postion of lvl 4
+        // Remove from here and keep in the isFinished method
         if (Math
             .abs(coralSubsystem.getElevatorEncoder() - CoralConstants.LEVEL_FOUR_HEIGHT) <= CoralConstants.ELEVATOR_TOLERANCE) {
             coralSubsystem.setElevatorSpeed(0);
@@ -41,18 +44,23 @@ public class ScoreL4Command extends LoggingCommand {
         if (Math
             .abs(coralSubsystem.getElevatorEncoder() - CoralConstants.LEVEL_FOUR_HEIGHT) <= CoralConstants.ELEVATOR_TOLERANCE) {
             // logs that it's at lvl 4 if the position is at lvl 4
+            // FIXME: use setFinishReason("reason text");
+            // the logging will happen in the end routine.
+
             log("AT L4");
             return true;
         }
-
-        else
+        else {
+            // FIXME: no else is required, do not leave empty else blocks hanging
             ;
+        }
         return false;
 
     }
 
     @Override
     public void end(boolean interrupted) {
+        // FIXME At the end, stop the elevator
         logCommandEnd(interrupted);
     }
 }
